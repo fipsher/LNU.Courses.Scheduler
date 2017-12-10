3fc96d77ae39dd74c54b211fb16fffb1c189f80e
Andrii Zvarych Tarasovuch <fipsher@gmail.com>
2017-12-10 19:46:01 +0200

    Add project files.

---
+++ LNU.TickTock/IExecutableJob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LNU.TickTock
+{
+    public interface IExecutableJob
+    {
+        /// <summary>
+        /// Run method
+        /// </summary>
+        /// <returns></returns>
+        Task Execute();
+    }
+}
---
+++ LNU.TickTock/IScheduledService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LNU.TickTock
+{
+    public interface IScheduledService
+    {
+        /// <summary>
+        /// Check is it a time to start deadline logic
+        /// </summary>
+        /// <returns>
+        /// None if time is not came. 
+        /// First if it is a time to launch First deadline logic.
+        /// Second if it is a time to launch Second deadline job
+        /// </returns>
+        Task<Enums.DeadlineType> Check();
+
+
+        /// <summary>
+        /// Run deadline job
+        /// </summary>
+        /// <param name="job">First of second deadline job</param>
+        /// <returns></returns>
+        Task Run(IExecutableJob job);
+    }
+}
